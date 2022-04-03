cabc76d109ea2451238e6b27f48a71c1a27b8dcd
Jayanth Koppala <jayant.sms.in@gmail.com>
2022-04-03 19:26:29 +0530

    Changing reducer.js

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const fn = (state, action) => {
     switch(action.type) {
       case 'DELETE_TRANSACTION':
         return {
@@ -14,3 +14,4 @@ export default (state, action) => {
         return state;
     }
   }
+  export default fn;
